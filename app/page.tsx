53ceb4bfb4abb26f46232b1f08dafaf655ae6f5c
azlinliana <azlinliana.adlan@gmail.com>
2025-06-05 21:52:58 +0800

    Update page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,33 @@
+import { SectionWrapper } from "@/components/ui/section-wrapper";
+
+import { Navbar } from "@/components/navbar";
+
 import { Hero } from "@/components/hero";
+import { About } from "@/components/about";
+import { Skills } from "@/components/skills";
+import { Projects } from "@/components/projects";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative flex justify-center items-center flex-col mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <Hero />
+        <Navbar />
+
+        <SectionWrapper id="hero">
+          <Hero />
+        </SectionWrapper>
+
+        <SectionWrapper id="about">
+          <About />
+        </SectionWrapper>
+
+        <SectionWrapper id="skills">
+          <Skills />
+        </SectionWrapper>
+
+        <SectionWrapper id="projects">
+          <Projects />
+        </SectionWrapper>
       </div>
     </main>
   );
