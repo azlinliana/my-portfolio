620fb0bf29542bb747deb6ba75209a6e87ed8e24
azlinliana <azlinliana.adlan@gmail.com>
2025-06-10 23:27:06 +0800

    Add timeline component

--- components/ui/timeline.tsx
+++ components/ui/timeline.tsx
@@ -34,17 +34,17 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
 
   return (
     <div
-      className="w-full bg-white dark:bg-neutral-950 font-sans md:px-10"
+      className="w-full bg-white dark:bg-black font-sans md:px-10"
       ref={containerRef}
     >
-      <div className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10">
-        <h2 className="text-lg md:text-4xl mb-4 text-black dark:text-white max-w-4xl">
+      {/* <div className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10">
+        <h2 className="text-lg md:text-4xl mb-4 text-black dark:text-white max-w-4xl text-justify">
           Professional Experience
         </h2>
-        <p className="text-neutral-700 dark:text-neutral-300 text-sm md:text-base max-w-sm">
+        <p className="text-neutral-700 dark:text-neutral-300 text-sm md:text-base max-w-sm text-justify">
           With close to 3 years of experience in web development, I've worked on backend and full-stack projects that taught me how to write clean, scalable code and collaborate across teams. Here's a look at my journey so far.
         </p>
-      </div>
+      </div> */}
 
       <div ref={ref} className="relative max-w-7xl mx-auto pb-20">
         {data.map((item, index) => (
