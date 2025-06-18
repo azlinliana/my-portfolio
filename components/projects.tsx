d8995e382215d6ea4008dc17c8981f08340c67c0
azlinliana <azlinliana.adlan@gmail.com>
2025-06-18 14:55:25 +0800

    Update projects component

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,6 +1,12 @@
 import { TextHoverEffect } from "./ui/text-hover-effect";
 import { PinContainer } from "./ui/3d-pin";
 
+import Image from "next/image";
+import portfolioCover from "../public/projects/my-portfolio/my-portfolio-cover.png";
+import sdmsCover from "../public/projects/sdms/sdms-cover.png";
+import mintyCover from "../public/projects/minty/minty-cover.png";
+import medretCover from "../public/projects/medret/medret-cover.png";
+
 export function Projects() {
   return (
     <>
@@ -11,35 +17,79 @@ export function Projects() {
       {/* Cards Grid */}
       <div className="w-full flex flex-wrap gap-8 justify-center sm:justify-start">
         {/* Card */}
-        <PinContainer title="/azlinliana.com" href="/azlinliana.com">
+        <PinContainer title="github/my-portfolio" href="https://github.com/azlinliana/my-portfolio.git">
           <div className="flex flex-col p-4 tracking-tight text-slate-100/50 w-[20rem] h-[20rem]">
             <h3 className="font-bold text-base text-slate-100 mb-2">My Portfolio</h3>
-            <p className="text-base text-slate-500 mb-4">My web developer resume.</p>
-            <div className="flex-1 w-full rounded-lg bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
+
+            <p className="text-base text-slate-500 mb-4">
+              My web developer resume.
+            </p>
+
+            <div className="flex-1 w-full rounded-lg relative overflow-hidden">
+              <Image
+                src={portfolioCover}
+                alt="Portfolio Cover"
+                fill
+                priority
+                className="object-cover"
+              />
+            </div>
           </div>
         </PinContainer>
 
-        <PinContainer title="/sdms.project" href="/sdms.project">
+        <PinContainer title="/github/sdms" href="https://github.com/nabbysidek/sdms.git">
           <div className="flex flex-col p-4 tracking-tight text-slate-100/50 w-[20rem] h-[20rem]">
             <h3 className="font-bold text-base text-slate-100 mb-2">Student Disciplinary Management System</h3>
-            <p className="text-base text-slate-500 mb-4">Lorem ipsum.</p>
-            <div className="flex-1 w-full rounded-lg bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
+
+            <p className="text-base text-slate-500 mb-4">
+              Manage student discipline records.
+            </p>
+
+            <div className="flex-1 w-full rounded-lg relative overflow-hidden">
+              <Image
+                src={sdmsCover}
+                alt="Portfolio Cover"
+                fill
+                priority
+                className="object-cover"
+              />
+            </div>          
           </div>
         </PinContainer>
 
-        <PinContainer title="/minty.project" href="/minty.project">
+        <PinContainer title="/github/minty" href="https://github.com/azlinliana/minty-frontend.git">
           <div className="flex flex-col p-4 tracking-tight text-slate-100/50 w-[20rem] h-[20rem]">
             <h3 className="font-bold text-base text-slate-100 mb-2">Minty</h3>
-            <p className="text-base text-slate-500 mb-4">Lorem ipsum.</p>
-            <div className="flex-1 w-full rounded-lg bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
+
+            <p className="text-base text-slate-500 mb-4">Manage personal finance.</p>
+
+            <div className="flex-1 w-full rounded-lg relative overflow-hidden">
+              <Image
+                src={mintyCover}
+                alt="Minty Cover"
+                fill
+                priority
+                className="object-cover"
+              />
+            </div>  
           </div>
         </PinContainer>
 
-        <PinContainer title="/other.project" href="/other.project">
+        <PinContainer title="/github/medret" href="https://github.com/azlinliana/medret.git">
           <div className="flex flex-col p-4 tracking-tight text-slate-100/50 w-[20rem] h-[20rem]">
-            <h3 className="font-bold text-base text-slate-100 mb-2">Other</h3>
-            <p className="text-base text-slate-500 mb-4">Lorem ipsum.</p>
-            <div className="flex-1 w-full rounded-lg bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
+            <h3 className="font-bold text-base text-slate-100 mb-2">Medication Reminder & Tracker Application</h3>
+
+            <p className="text-base text-slate-500 mb-4">Medication reminder application.</p>
+
+            <div className="flex-1 w-full rounded-lg relative overflow-hidden">
+              <Image
+                src={medretCover}
+                alt="Minty Cover"
+                fill
+                priority
+                className="object-cover"
+              />
+            </div>  
           </div>
         </PinContainer>
       </div>
