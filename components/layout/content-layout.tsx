3c4ae3bc2d9827d04789f04136ad8043947c1474
Nur Azlin Liana binti Mohd Adlan <azlinliana.adlan@gmail.com>
2025-08-07 11:57:04 +0800

    Content layout

---
+++ components/layout/content-layout.tsx
@@ -0,0 +1,7 @@
+export default function ContentLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <main className="min-h-screen px-10 sm:px-30 py-20">
+            {children}
+        </main>
+    );
+}
