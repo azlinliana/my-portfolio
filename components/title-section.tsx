21aa229d90be82b214008608f689e8edc122f30b
Nur Azlin Liana binti Mohd Adlan <azlinliana.adlan@gmail.com>
2025-08-07 15:07:40 +0800

    Component title section

---
+++ components/title-section.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { TextHoverEffect } from "./ui/text-hover-effect";
+
+type TitleProps = {
+  text: string;
+  className?: string;
+};
+
+export default function TitleSection({ text, className = "" }: TitleProps) {
+  return (
+    <div className={`h-[8rem] flex items-center justify-center ${className}`}>
+      <TextHoverEffect text={text} />
+    </div>
+  );
+}
