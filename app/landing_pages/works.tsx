e039620626d1ce57bd8ae5c2768afd53d9ceac57
Nur Azlin Liana binti Mohd Adlan <azlinliana.adlan@gmail.com>
2025-09-11 18:03:12 +0800

    Add work page

---
+++ app/landing_pages/works.tsx
@@ -0,0 +1,10 @@
+import ContentLayout from "@/components/layout/content-layout";
+import TitleSection from "@/components/title-section";
+
+export default function Works() {
+    return (
+        <ContentLayout>
+            <TitleSection text="W O R K S" />
+        </ContentLayout>
+    );
+}
