f14af6575a53e290be12d02b7689b996a105ea8a
Nur Azlin Liana binti Mohd Adlan <azlinliana.adlan@gmail.com>
2025-09-11 18:03:01 +0800

    Update app page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { Navbar } from "@/components/navbar";
 import Hero from "./landing_pages/hero";
 import About from "./landing_pages/about";
+import Skills from "./landing_pages/skills";
+import Projects from "./landing_pages/projects"
 
 export default function Page() {
   return (
@@ -10,6 +12,10 @@ export default function Page() {
       <Hero />
 
       <About />
+
+      <Skills />
+
+      <Projects />
     </>
   );
 }
